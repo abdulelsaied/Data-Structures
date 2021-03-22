0a3e5b49eeaec46671b23c11429c882c453a522a
Abdul El-Saied <43714080+abdulelsaied@users.noreply.github.com>
2021-03-22 09:40:48 -0700

    Update BitExercise.java

--- BitExercise.java
+++ BitExercise.java
@@ -3,16 +3,6 @@
  */
 
 public class BitExercise {
-    
-    /** Fill in the function below so that it returns 
-    * the value of the argument x with all but its last 
-    * (least significant) 1-bit set to 0.
-    * For example, 100 in binary is 0b1100100, so lastBit(100)
-    * should return 4, which in binary is 0b100.
-    */
-    public static int lastBit(int x) {
-        return 0;
-    }
 
     /** Fill in the function below so that it returns 
     * True iff x is a power of two, otherwise False.
